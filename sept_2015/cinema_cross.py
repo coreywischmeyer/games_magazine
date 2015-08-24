b4f87aceb1163f4076054073f866c39e58951177
Corey Wischmeyer <corey.wischmeyer@gmail.com>
2015-08-23 21:14:32 -0500

    Added some rules.
    Playing around with some possible rules. As the program stands it
    whittles down the the 3,000,000 title list down to ~6000. Still not good
    enough.

--- sept_2015/cinema_cross.py
+++ sept_2015/cinema_cross.py
@@ -1,13 +1,43 @@
+import re
 __author__ = 'Corey Wischmeyer'
 
-
+IMDB_FILE = "movies.list"
 MOVIE_TITLE_LENGTH = 17
 MOVIE_TITLE_WORDS = 3
 
-letters = set(['A', 'B', 'D', 'E', 'F',
-               'G', 'H', 'I', 'J', 'L',
-               'M', 'N', 'O', 'P', 'R',
-               'S', 'T', 'U', 'V',
-               'W', 'Y'])
+TITLE_SET = set()
 
-print(letters)
+letters = {'A', 'B', 'D', 'E', 'F',
+           'G', 'H', 'I', 'J', 'L',
+           'M', 'N', 'O', 'P', 'R',
+           'S', 'T', 'U', 'V',
+           'W', 'Y', '\'', ' '}
+
+def process_title(title):
+    #Take in a title, get the title bit, (before the year)
+    #Remove the Parens
+    #Get only unique titles
+    pattern = re.compile("(.*)?\(\d")
+    if re.match(pattern, title):
+        m = pattern.findall(title)[0]
+        m = m.replace('"', '')
+        if is_ascii(m) and len(m.split()) == 3\
+                and letters.issuperset(set(m.upper()))\
+                and count_letters(m) == 17:
+            return m
+
+def count_letters(word):
+    return len(word) - word.count(' ') - word.count('\'')
+
+def is_ascii(s):
+    return all(ord(c) < 128 for c in s)
+
+
+with open(IMDB_FILE, 'r') as f:
+    for item in f:
+        TITLE_SET.add(process_title(item))
+
+for i in TITLE_SET:
+    print(i)
+
+
